53f9c6bd025416fd6d11bb390ad3ad973b7d9d7a
pronghe <pronghe@LAP-PRONGHE.elogex.com>
2020-09-17 07:00:44 +0530

    DNAP-13 Lombok not working .

--- backend/src/main/java/com/dna/backend/BackendApplication.java
+++ backend/src/main/java/com/dna/backend/BackendApplication.java
@@ -1,12 +1,14 @@
 package com.dna.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableAutoConfiguration
 public class BackendApplication {
 
 	public static void main(String[] args) {
