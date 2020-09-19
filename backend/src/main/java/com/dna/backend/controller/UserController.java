f4011f4e5c8e0729f1d1d4c3d181d34f535fa2b8
pronghe <pronghe@LAP-PRONGHE.elogex.com>
2020-09-19 05:39:53 +0530

    Reversing bad commit for DI.

--- backend/src/main/java/com/dna/backend/controller/UserController.java
+++ backend/src/main/java/com/dna/backend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.dna.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,63 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dna.backend.modle.User;
 import com.dna.backend.repository.UserRepository;
+
 //www.google.com/user/1 ---- path param
 @RequestMapping("/user")
 @RestController
 @CrossOrigin
-@Component
 public class UserController {
 
 	@Autowired // DI from spring
 	private UserRepository userRepository;
-	private User Abc;
-	
-	
+
 	@GetMapping("/")
-	public List<User> getAllUsers(){
+	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
-	public User getUser(@PathVariable Integer id){
+	public User getUser(@PathVariable Integer id) {
 		return userRepository.findById(id).orElse(null);
 	}
-	
+
 	@PostMapping("/")
 	public User postUser(@RequestBody User user) {
 		return userRepository.save(user);
 
 	}
+
 	@DeleteMapping("/{id}")
 	public void deleteUser(@PathVariable Integer id) {
-		 userRepository.deleteById(id);
+		userRepository.deleteById(id);
 	}
-	@PatchMapping("/")
-	public User patUser (@RequestBody User user) {
-		return userRepository.save(user);
-	}
-	
-	/*
-	 * @Bean("/") public User getUser(@RequestBody User user) { return
-	 * userRepository.save(user);
-	 */
-	
-	public void display() {
-		System.out.println("this is display");
-		
-		
-	}
-	
-	
-	
-	
+
 	@PutMapping("/")
 	public User putUser(@RequestBody User user) {
 		return userRepository.save(user);
 	}
+
 	@PatchMapping("/")
 	public User patchUser(@RequestBody User user) {
 		return userRepository.save(user);
 	}
-	
+
 }
--- backend/src/main/java/com/dna/backend/modle/User.java
+++ backend/src/main/java/com/dna/backend/modle/User.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import org.springframework.stereotype.Component;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +14,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
-
 
 public class User {
 
