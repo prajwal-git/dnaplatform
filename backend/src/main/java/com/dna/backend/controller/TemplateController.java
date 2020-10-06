0ca09e9e56bdd6be6c1b122f2166709b12560b7f
pronghe <pronghe@LAP-PRONGHE.elogex.com>
2020-10-06 10:37:11 +0530

    Spring security Changes added

--- backend/src/main/java/com/dna/backend/controller/TemplateController.java
+++ backend/src/main/java/com/dna/backend/controller/TemplateController.java
@@ -1,33 +1,20 @@
 package com.dna.backend.controller;
 
-import java.security.Principal;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 
 
-@RequestMapping("/")
-@RestController
+@Controller
 public class TemplateController {
 	
 	@GetMapping("/login")
-	public String getLoginView() {
+	public String login() {
 		return "login";
 	}
 	
-	@RequestMapping("user")
-	@ResponseBody
-	public Principal getUser(Principal principal) {
-		return principal;
-	}
-	
-	
-	@GetMapping("/login-success")
-	public String getCourses() {
+	@GetMapping("/")
+	public String home() {
 		return "index";
 	}
-
 }
