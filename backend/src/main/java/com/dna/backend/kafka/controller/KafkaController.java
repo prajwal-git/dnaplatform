74e9c66a152fa1cda52a1b994a4e6f399243e6e1
pronghe <pronghe@LAP-PRONGHE.elogex.com>
2021-02-10 09:05:38 +0530

    Adding Kafka to DNA

---
+++ backend/src/main/java/com/dna/backend/kafka/controller/KafkaController.java
@@ -0,0 +1,26 @@
+package com.dna.backend.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dna.backend.kafka.producer.Producer;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
