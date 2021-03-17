b5eb3252c942950f8cf0872d69841f9e40edfcf7
umesh shrestha <umesh.state@gmail.com>
2021-03-16 23:39:55 -0500

    DNA-46 Log in page is missing.

--- backend/src/main/java/com/dna/backend/BackendApplication.java
+++ backend/src/main/java/com/dna/backend/BackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication // this is bootstrap classloader this is load first
 @EnableSwagger2
 @EnableAutoConfiguration
 public class BackendApplication {
